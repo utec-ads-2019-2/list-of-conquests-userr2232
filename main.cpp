1f032bea6fc44ea9f468f68e12f28c807ddcd586
userr2232 <reynaldo.rz.26@gmail.com>
2019-08-22 18:42:54 -0500

    attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-
+#include <map>
+#include <sstream> 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main() {
+    map<string,int> conquests;
+    string line, country;
+    int N;
+    cin >> N;
+    getline(cin,line);
+    for(int i = 0; i < N; ++i) {
+        getline(cin,line);
+        istringstream iss(line);
+        iss >> country;
+        conquests[country] = conquests.count(country) ? conquests[country] + 1 : 1;
+    }
+    for(map<string,int>::iterator it = conquests.begin(); it != conquests.end(); it++)
+        cout << it->first << " " << it->second << endl;
+	return 0;
 }
